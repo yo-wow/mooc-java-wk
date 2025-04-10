bf92d5e5a8cc2d5b505c8c040fd04fca0c10eaf9
yo-wow <19966441212@126.com>
2025-04-10 15:55:10 +0800

    使用封装降低耦合性

--- castle/src/castle/Game.java
+++ castle/src/castle/Game.java
@@ -41,14 +41,7 @@ public class Game {
     private void showPrompt() {
         System.out.println("现在你在" + currentRoom);
         System.out.print("出口有：");
-        if (currentRoom.northExit != null)
-            System.out.print("north ");
-        if (currentRoom.eastExit != null)
-            System.out.print("east ");
-        if (currentRoom.southExit != null)
-            System.out.print("south ");
-        if (currentRoom.westExit != null)
-            System.out.print("west ");
+        System.out.print(currentRoom.getRoomDirection());
         System.out.println();
     }
 
@@ -60,20 +53,7 @@ public class Game {
     }
 
     private void goRoom(String direction) {
-        Room nextRoom = null;
-        if (direction.equals("north")) {
-            nextRoom = currentRoom.northExit;
-        }
-        if (direction.equals("east")) {
-            nextRoom = currentRoom.eastExit;
-        }
-        if (direction.equals("south")) {
-            nextRoom = currentRoom.southExit;
-        }
-        if (direction.equals("west")) {
-            nextRoom = currentRoom.westExit;
-        }
-
+        Room nextRoom = currentRoom.getNextRoom(direction);
         if (nextRoom == null) {
             System.out.println("那里没有门！");
         } else {
--- castle/src/castle/Room.java
+++ castle/src/castle/Room.java
@@ -1,32 +1,58 @@
 package castle;
 
 public class Room {
-    public String description;
-    public Room northExit;
-    public Room southExit;
-    public Room eastExit;
-    public Room westExit;
+    private String description;
+    private Room northExit;
+    private Room southExit;
+    private Room eastExit;
+    private Room westExit;
 
-    public Room(String description) 
-    {
+    public Room(String description) {
         this.description = description;
     }
 
-    public void setExits(Room north, Room east, Room south, Room west) 
-    {
-        if(north != null)
+    public void setExits(Room north, Room east, Room south, Room west) {
+        if (north != null)
             northExit = north;
-        if(east != null)
+        if (east != null)
             eastExit = east;
-        if(south != null)
+        if (south != null)
             southExit = south;
-        if(west != null)
+        if (west != null)
             westExit = west;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return description;
+    }
+
+    public String getRoomDirection() {
+        StringBuilder sb = new StringBuilder();
+        if (this.northExit != null)
+            sb.append("north ");
+        if (this.eastExit != null)
+            sb.append("east ");
+        if (this.southExit != null)
+            sb.append("south ");
+        if (this.westExit != null)
+            sb.append("west ");
+        return sb.toString();
+    }
+
+    public Room getNextRoom(String direction) {
+        if (direction.equals("north")) {
+            return this.northExit;
+        }
+        if (direction.equals("east")) {
+            return this.eastExit;
+        }
+        if (direction.equals("south")) {
+            return this.southExit;
+        }
+        if (direction.equals("west")) {
+            return this.westExit;
+        }
+        return null;
     }
 }
